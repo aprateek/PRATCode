7d90dd6284e58719d5110ab4f42f672b43b02b2d
aprateek <ag.prateek@gmail.com>
2017-10-27 21:50:06 +0530

    addting l DB

---
+++ lambdaDB/rds_config.py
@@ -0,0 +1,5 @@
+#config file containing credentials for rds mysql instance
+db_username = "prateek"
+db_password = "pratruchi"
+db_name = "exampleDB"
+db_endpoint= "pratdbinst.cuahsi78y7vf.ap-south-1.rds.amazonaws.com" 
