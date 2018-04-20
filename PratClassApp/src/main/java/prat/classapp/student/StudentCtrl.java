e57072347a8089f9e52417203ecd25cf02af530d
aprateek <ag.prateek@gmail.com>
2018-04-20 19:32:29 +0530

    added modify and delete

--- PratClassApp/src/main/java/prat/classapp/student/StudentCtrl.java
+++ PratClassApp/src/main/java/prat/classapp/student/StudentCtrl.java
@@ -11,7 +11,10 @@ import javax.validation.Valid;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,18 +30,11 @@ public class StudentCtrl {
 	
 	@Autowired
 	private StudentRepo studRepo;
-	
 	private static final Logger logger = LogManager.getLogger(InstructorCtrl.class);
 	
-	@RequestMapping(method=RequestMethod.POST,value ="/students")
-	public Student createStudent(@RequestBody Student student) {
-		student =  setStudent();
-		
-		logger.info("Student Date "+ student.getCreateDate());
-		logger.info("Student Name "+ student.getStudFname());
-		logger.info("Student DOC "+ student.getDob());
-	    return studRepo.save(student);
-	}
+	
+	 
+	// ------------------- Get a Student-----------------------------------------
 	@RequestMapping(value = "/student/{id}")
 	public @ResponseBody String showStudent(@RequestParam int id) {
 		 
@@ -48,7 +44,54 @@ public class StudentCtrl {
 		return name;
 
 	}
+	// ------------------- Add a Student-----------------------------------------t 
+		@RequestMapping(method=RequestMethod.POST,value ="/students")
+		public Student createStudent(@RequestBody Student student) {
+			student =  setStudent();
+			
+			logger.info("Student Date "+ student.getCreateDate());
+			logger.info("Student Name "+ student.getStudFname());
+			logger.info("Student DOC "+ student.getDob());
+		    return studRepo.save(student);
+		}
+		
+	// ------------------- modify a Student-----------------------------------------  
+	@RequestMapping(value = "/student/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<?> updateUser(@RequestParam int id, @RequestBody Student stud) {
+        logger.info("Updating student with id {}", id);
+        stud = setStudent();
+        Student currentUser = studRepo.findByStudID(id);
+        logger.info( " Mdifying",currentUser.getStudFname());
+        
+        if (currentUser == null) {
+            logger.error("Unable to update. User with id {} not found.", id);
+            return new ResponseEntity( "Student Not Found",HttpStatus.NOT_FOUND);
+        }
+         currentUser.setStudFname(stud.getStudFname());
+        //currentUser.setDob(stud.getDob());
+        currentUser.setUpdateBy(stud.getUpdateBy());;
+ 
+        studRepo.save(currentUser);
+        return new ResponseEntity<Student>(currentUser, HttpStatus.OK);
+    }
+ 
+	// ------------------- Delete a Student-----------------------------------------
+	 
+    @RequestMapping(value = "/student/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteStudent(@RequestParam int id) {
+        logger.info("Fetching & Deleting User with id {}", id);
+ 
+        Student user = studRepo.findByStudID(id);
+        if (user == null) {
+            logger.error("Unable to delete. User with id {} not found.", id);
+            return new ResponseEntity( ("Unable to delete. User with id " + id + " not found."),
+                    HttpStatus.NOT_FOUND);
+        }
+        studRepo.deleteById(id);
+        return new ResponseEntity<Student>(HttpStatus.NO_CONTENT);
+    }
 	
+	// create a dummy student 
 	private Student setStudent(){
 		
 		Student stud = new Student();
@@ -61,8 +104,8 @@ public class StudentCtrl {
 			e.printStackTrace();
 		} 
 		
-		stud.setStudID(345);
-		stud.setStudFname ("Prateek");
+		//stud.setStudID(112);
+		stud.setStudFname ("ANNNN");
 		stud.setStudLName("Agrawal");
 		stud.setActive(true);
 		stud.setDob(date );
@@ -70,7 +113,7 @@ public class StudentCtrl {
 		stud.setSex('F');
 		stud.setCreateDate(date);
 		stud.setUpdateDate(date);
-		
+		stud.setUpdateBy("HONOOO");
 		
 		return stud;
 	}
